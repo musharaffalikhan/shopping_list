6f202c523b5a90ce230f5f670d84ee3c6b0b1aa8
Musharaff Ali Khan <musharaffalikhan@gmail.com>
2023-06-12 14:49:24 +0530

    Added dismissable delete functionality

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,4 @@
-import 'dart:js_util';
-
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_data.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -29,38 +26,49 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  // void removeItem(GroceryItem item) {
-  //   setState(() {
-  //     _groceryItems.remove(item);
-  //   });
-  // }
+  void removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
 
   @override
   Widget build(context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Groceries"),
-        actions: [
-          IconButton(
-            onPressed: _addItem,
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
+    Widget content = const Center(
+      child: Text("No item added yet!"),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Your Groceries"),
+          actions: [
+            IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: content);
   }
 }
