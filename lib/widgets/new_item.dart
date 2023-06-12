7e446c47a33b6176a7724d2e384290649e9bc68e
Musharaff Ali Khan <musharaffalikhan@gmail.com>
2023-06-12 12:23:09 +0530

    first commit

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add new item"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text("Name"),
+                ),
+                validator: (value) {},
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Quantity"),
+                      ),
+                      initialValue: "1",
+                      keyboardType: TextInputType.number,
+                      validator: (value) {},
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 16,
+                                width: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 21,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: const Text("Reset")),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("Add Expense"))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
